8cfce7205b7523bcb8019ac7a10b441139033480
tbrzezinski1 <tbrzezinski1@gmail.com>
2025-07-17 15:34:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,5 @@ if ingredients_list:
 
 # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json)
+# st.text(smoothiefroot_response.json)
 sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
